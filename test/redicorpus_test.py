828117301280788e318fc19a268538b21fb4f5f7
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-02-16 18:42:49 -0800

    one test added

--- test/redicorpus_test.py
+++ test/redicorpus_test.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 
+import json
 import pytest
-import redicorpus as rc
+from redicorpus.base import redicorpus
+
+def test_string():
+    obj = redicorpus.String('try')
+    assert obj.__to_tuple__() == ('try', 'try', 'NN', None)
+
+# TODO insert tests for other StringLike
