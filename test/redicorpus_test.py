1b724c1f638dfeff0b9e9a4de0f13f927e7f461c
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-02-21 20:56:30 -0800

    more DictLike tests

--- test/redicorpus_test.py
+++ test/redicorpus_test.py
@@ -18,6 +18,11 @@ def test_lemma():
     obj = rc.Lemma('fried')
     assert obj.__to_tuple__() == ('fry', 'fried', 'VBN', 'Lemma')
 
+def test_dict_like():
+    obj = rc.DictLike()
+    obj['test'] = 42
+    assert list(obj.items()) == [('test', 42)]
+
 def test_comment():
     with open('data/comment.json', 'r') as f:
         data = json.load(f)
@@ -27,5 +32,34 @@ def test_comment():
     assert comment['_id'] == 'd024gzv'
     r = comment.insert()
 
-for collection in c['test'].collection_names():
-    c['test'].drop_collection(collection)
+def test_update_body():
+    for str_type in rc.StringLike.__subclasses__():
+        document = c['test'][str_type.__name__].find_one()
+        assert document
+        assert len(document['users']) == 1
+        assert document['count'] == len(document['polarity'])
+
+def test_update_dictionary():
+    counter = c['1gram']['counters'].find_one({'str_type' : 'Lemma'})
+    assert counter
+    assert counter['counter'] > 1
+    document = c['1gram']['Lemma']
+    assert document
+    assert document['_id'] < counter['counter']
+
+def test_update_source():
+    document = c['test']['Comment'].find_one({'_id' : "d024gzv"})
+    assert document
+
+def test_array_like():
+    pass
+
+def test_body():
+    pass
+
+def test_map():
+    pass
+
+def test_cleanup():
+    for collection in c['test'].collection_names():
+        c['test'].drop_collection(collection)
