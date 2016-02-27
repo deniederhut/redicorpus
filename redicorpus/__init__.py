5ae366e0e3c94cccc93aed49d2e25e8f41a104c3
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-02-27 12:13:01 -0800

    specifying collection options in __init__

--- redicorpus/__init__.py
+++ redicorpus/__init__.py
@@ -1,10 +1,43 @@
 #!/usr/bin/env python
 
-from pymongo import MongoClient
+import pymongo
 from redicorpus.celery import app
 
-c = MongoClient(j=True)
-assert any(c.database_names())
+ngram_collections = ['1gram', '2gram', '3gram']
+str_databases = ['String', 'Stem', 'Lemma']
+
+# Initializing Mongo Databases
+
+c = pymongo.MongoClient()
+
+for collection in ngram_collections:
+    try:
+        c['Counter'].create_collection(collection,w=2)
+    except pymongo.errors.CollectionInvalid:
+        pass
+    c['Counter'][collection].create_indexes([
+        pymongo.IndexModel(
+            [('str_type', pymongo.TEXT)], unique=True, background=False
+        )
+    ])
+
+for database in str_databases:
+    for collection_name in ngram_collections:
+        try:
+            c[database].create_collection(collection, w=2)
+        except pymongo.errors.CollectionInvalid:
+            pass
+        c[database][collection].create_indexes([
+            pymongo.IndexModel(
+                [('_id', pymongo.ASCENDING)], unique=True, background=False
+            ),
+            pymongo.IndexModel(
+                [('term', pymongo.TEXT)], unique=True, background=False
+            )
+        ])
+
+
+# Checking celery
 
 @app.task
 def celery_running():
