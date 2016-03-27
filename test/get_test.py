6cc577296d950b4d8aaf96e19185625adec2deaf
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-03-26 22:39:12 -0700

    basic architecture for get test

---
+++ test/get_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import pytest
+from redicorpus.get import askreddit
+
+def test_client():
+    pass
+
+def test_response():
+    pass
+
+def test_get_datelimit():
+    pass
+
+def test_set_datelimit():
+    pass
+
+def test_
