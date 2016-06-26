ee3230202068a84329b5a804139e2428511b2275
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-06-25 17:52:31 -0700

    switches results backend from rabbitmq to sqlite

--- redicorpus/celery.py
+++ redicorpus/celery.py
@@ -6,17 +6,18 @@ from celery import Celery
 
 app = Celery('redicorpus',
              broker='amqp://',
-             backend='rpc://',
+             backend='db+sqlite:///tmp_results.sqlite',
              include=[
                 'redicorpus',
                 'redicorpus.objects',
-                 'test'
+                'test'
                  ]
              )
 
 # Optional configuration, see the application user guide.
 app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=3600,
+    CELERY_TASK_RESULT_EXPIRES = 3600,
+    CELERY_MAX_CACHED_RESULTS = 1000,
     CELERY_ACCEPT_CONTENT = ['pickle']
 )
 
