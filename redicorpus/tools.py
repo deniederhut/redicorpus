9398e5e8d67a06155dd9bd4308f98f71993cde33
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-06-25 23:07:35 -0700

    adds function to split time into days and testing

--- redicorpus/tools.py
+++ redicorpus/tools.py
@@ -1,6 +1,8 @@
 #!/bin/env python
 
+from arrow import Arrow
 import re
+from datetime import datetime, timedelta
 
 def parse_markdown(text):
     link_list = []
@@ -22,3 +24,19 @@ def pos_to_wordnet(pos):
         return 'a'
     else:
         return 'n'
+
+def split_time(start_date, stop_date):
+    """
+    Split start and stop datetime objects into a period of whole days, and the remainder on either end, and return it as a dictionary
+    """
+    result = {}
+    result['start_day'] = Arrow(
+        start_date.year, start_date.month, start_date.day
+    ).datetime + timedelta(1)
+    result['remainder_start'] = result['start_day'] - start_date
+    result['stop_day'] = Arrow(
+        stop_date.year, stop_date.month, stop_date.day
+    ).datetime
+    result['remainder_stop'] = stop_date - result['stop_day']
+    result['n_days'] = (result['stop_day'] - result['start_day']).days
+    return result
--- test/tools_test.py
+++ test/tools_test.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import
 
+from arrow import Arrow
+from datetime import datetime, timedelta
 import pytest
 from redicorpus import tools
 
@@ -10,3 +12,19 @@ def test_parse_markdown():
     text, links = tools.parse_markdown(data)
     assert text == "Trump's wall just got 10 feet higher! \n\n#Total height: 70ft. \n\n***** \n\nBot by /u/TonySesek556"
     assert links == ['https://youtu.be/gPfJwc8Cwao?t=19s']
+
+def test_pos():
+    assert tools.pos_to_wordnet('VB') == 'v'
+    assert tools.pos_to_wordnet('RB') == 'r'
+    assert tools.pos_to_wordnet('JJ') == 'a'
+    assert tools.pos_to_wordnet('DILLON') == 'n'
+
+def test_split_time():
+    result = tools.split_time(
+        Arrow(1, 1, 1, 1, 1).datetime, Arrow(2, 2, 2, 2, 2).datetime
+    )
+    assert result['n_days'] == 396
+    assert result['remainder_start'] == timedelta(0, 82740)
+    assert result['remainder_stop'] == timedelta(0, 7320)
+    assert result['start_day'] == Arrow(1, 1, 2, 0, 0).datetime
+    assert result['stop_day'] == Arrow(2, 2, 2, 0, 0).datetime
